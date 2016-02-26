7fb5327bde3b1e58d21d84502eb0fb4a55fa4eb1
Esperanza Moreno Cruz <hola@mi-mina.com>
2016-02-26 19:53:44 +0100

    wip

--- src/todolist/core.clj
+++ src/todolist/core.clj
@@ -1,6 +1,32 @@
-(ns todolist.core)
+(ns todolist.core
+  (:require 
+            
+       
+            ;; ring.adapter.jetty is an adapter we can use in dev and production
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]
+            ;; wrap-params to parse the query-string and store the result 
+            ;; back into the request
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.file-info :refer [wrap-file-info]]
+            [compojure.core :refer [defroutes ANY GET POST PUT DELETE]]
+            [compojure.route :refer [not-found]]
+            ;; handle-dump helps us to see the request in a nice format
+            ;; It's useful as a debugging tool
+            [ring.handler.dump :refer [handle-dump]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn greet [req]
+  {:status 200
+   :body "Hello, world!"
+   :headers {}})
+
+(defroutes app
+  (GET "/" [] greet)
+  (not-found "Page not found"))
+
+(defn -main [port]
+  (jetty/run-jetty app {:port (Integer. port)}))
+
+(defn -dev-main [port]
+  (jetty/run-jetty (wrap-reload #'app) {:port (Integer. port)}))
